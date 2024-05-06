c31ad2fe94b3dc7d8f7883611220a61e3d30ccf7
Jahongir Hamidov <bright19.off@gmail.com>
2024-05-07 00:37:28 +0500

    contact page responsive

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -4,10 +4,9 @@ import qrcode from "../assets/img/qrcode.jpg";
 
 const ContactPage = () => {
   const [formData, setFormData] = useState({
-    from_name: "", // Matches {{from_name}} in your EmailJS template
-    user_email: "", // Matches an expected email placeholder, but you should have a placeholder for it in EmailJS
-    message: "", // Matches {{message}}
-    to_name: "br.developer19@gmail.com", // Assuming you want to send this to yourself or your business name
+    user_name: "",
+    user_email: "",
+    message: "",
   });
 
   const handleChange = (e) => {
@@ -16,24 +15,18 @@ const ContactPage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     emailjs
       .sendForm(
-        "service_pg1zt0b", // Your service ID
-        "template_mu1gxom", // Your template ID
-        e.target, // The form element
-        "vMRalq9p-Z-2v0CRO" // Your public key (user ID)
+        "service_pg1zt0b",
+        "template_mu1gxom",
+        e.target,
+        "vMRalq9p-Z-2v0CRO"
       )
       .then(
         (result) => {
           console.log("Email successfully sent!", result.text);
           alert("Message sent successfully!");
-          setFormData({
-            from_name: "",
-            user_email: "",
-            message: "",
-            to_name: "",
-          }); // Reset form after successful submission
+          setFormData({ user_name: "", user_email: "", message: "" });
         },
         (error) => {
           console.error("Failed to send email:", error.text);
@@ -45,8 +38,8 @@ const ContactPage = () => {
   return (
     <div className="container mx-auto px-4 pt-10">
       <h1 className="text-2xl font-bold mb-4">Contact</h1>
-      <div className="flex flex-row justify-between items-start">
-        <section className="flex flex-col gap-5">
+      <div className="flex flex-col lg:flex-row justify-between items-start">
+        <section className="flex flex-col gap-5 mb-5 lg:mb-0">
           <section className="flex flex-col p-10 gap-5 rounded-md bg-qrcode-gray">
             <img
               src={qrcode}
@@ -56,31 +49,40 @@ const ContactPage = () => {
             <span className="text-white max-w-[240px] font-bold">
               Telegramda Sharh qoldiring yoki savol bering
             </span>
-            <a href="https://t.me/maxwaymasterfood_bot" className="text-white">
-              @maxwaymasterfood_bot
-            </a>
+            <section>
+              <a
+                href="https://t.me/maxwaymasterfood_bot"
+                className="text-white"
+              >
+                @maxwaymasterfood_bot
+              </a>
+            </section>
           </section>
-          <span className="text-2xl font-bold mb-4">Yagona aloqa markazi</span>
-          <a href="tel:+998712005400">+998712005400</a>
+          <section className="flex flex-col">
+            <span className="text-2xl font-bold mb-4">
+              Yagona aloqa markazi
+            </span>
+            <a href="tel:+998712005400">+998712005400</a>
+          </section>
         </section>
 
         <form
-          className="flex flex-col bg-white w-[600px] shadow-lg rounded-lg p-8 ml-5"
+          className="flex flex-col bg-white w-full lg:w-2/5 shadow-lg rounded-lg p-8"
           onSubmit={handleSubmit}
         >
           <h2 className="text-xl font-semibold mb-4">Send us a message</h2>
           <div className="mb-4">
             <label
-              htmlFor="from_name"
+              htmlFor="user_name"
               className="block text-sm font-medium text-gray-700"
             >
               Name
             </label>
             <input
               type="text"
-              name="from_name"
-              id="from_name"
-              value={formData.from_name}
+              name="user_name"
+              id="user_name"
+              value={formData.user_name}
               onChange={handleChange}
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               required
