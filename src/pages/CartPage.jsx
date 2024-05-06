e3dcb4f999c2e675b3bd6cab01c5e11eed48bbb3
Jahongir Hamidov <bright19.off@gmail.com>
2024-05-07 01:52:15 +0500

    full project responsive

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -43,37 +43,37 @@ const CartPage = () => {
     setLoading(false);
   };
 
+  const getTotalPrice = () => {
+    return cartItems
+      .reduce((total, item) => total + item.price * item.quantity, 0)
+      .toFixed(2);
+  };
+
   const handleQuantityChange = (id, increment) => {
     const updatedItems = cartItems
       .map((item) => {
         if (item.id === id) {
           const newQuantity = item.quantity + (increment ? 1 : -1);
           if (newQuantity < 1) {
-            // Confirm removal if decrement leads to zero
             if (
               window.confirm("Do you want to remove this item from your cart?")
             ) {
-              handleRemoveItem(id); // Directly call remove function
-              return null; // Will be filtered out later
+              return null; // Will be filtered out
             }
-            return item; // Return unchanged if user cancels removal
+            return item; // Return unchanged if not removing
           }
           return { ...item, quantity: newQuantity };
         }
         return item;
       })
-      .filter((item) => item !== null); // Remove items set to null
-
+      .filter((item) => item); // Remove null items
     updateLocalStorage(updatedItems);
     setCartItems(updatedItems);
   };
 
   const updateLocalStorage = (items) => {
     const updatedStorage = items.reduce(
-      (acc, item) => ({
-        ...acc,
-        [item.id]: item.quantity,
-      }),
+      (acc, item) => ({ ...acc, [item.id]: item.quantity }),
       {}
     );
     localStorage.setItem("cartItems", JSON.stringify(updatedStorage));
@@ -89,53 +89,63 @@ const CartPage = () => {
   if (error) return <div>{error}</div>;
 
   return (
-    <div className="containerown px-4 pt-10">
+    <div className="container mx-auto px-4 pt-10">
       <h1 className="text-xl font-bold my-4">Cart Items</h1>
       {cartItems.length > 0 ? (
-        <table className="min-w-full table-fixed text-sm bg-white shadow-md rounded-lg">
-          <thead className="bg-gray-100">
-            <tr>
-              <th className="p-4 text-left">Product</th>
-              <th className="p-4 text-left">Description</th>
-              <th className="p-4 text-center">Price</th>
-              <th className="p-4 text-center">Quantity</th>
-              <th className="p-4 text-center">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cartItems.map((item) => (
-              <tr key={item.id} className="border-b">
-                <td className="p-4 flex items-center space-x-4">
-                  <img
-                    src={item.image}
-                    alt={item.name}
-                    className="h-20 w-20 object-cover rounded-md"
-                  />
-                  <div>
-                    <div className="font-medium text-gray-800">{item.name}</div>
-                  </div>
-                </td>
-                <td className="p-4">{item.description}</td>
-                <td className="p-4 text-center">${item.price}</td>
-                <td className="p-4 text-center">{item.quantity}</td>
-                <td className="p-4 text-center flex space-x-2 justify-center">
-                  <button
-                    className="bg-blue-500 text-white rounded-full h-10 w-10 flex justify-center items-center hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-700"
-                    onClick={() => handleQuantityChange(item.id, true)}
-                  >
-                    +
-                  </button>
-                  <button
-                    className="bg-red-500 text-white rounded-full h-10 w-10 flex justify-center items-center hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-700"
-                    onClick={() => handleQuantityChange(item.id, false)}
-                  >
-                    -
-                  </button>
-                </td>
+        <div>
+          <table className="min-w-full table-auto text-sm bg-white shadow-md rounded-lg">
+            <thead className="bg-gray-100">
+              <tr>
+                <th className="p-4 text-left">Product</th>
+                <th className="p-4 text-left hidden lg:block">Description</th>
+                <th className="p-4 text-center">Price</th>
+                <th className="p-4 text-center">Qty</th>
+                <th className="p-4 text-center">Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {cartItems.map((item) => (
+                <tr key={item.id} className="border-b">
+                  <td className="p-4 flex items-center flex-col sm:flex-row sm:space-x-4">
+                    <img
+                      src={item.image}
+                      alt={item.name}
+                      className="h-20 w-20 object-cover rounded-md"
+                    />
+                    <div>
+                      <div className="font-medium text-gray-800">
+                        {item.name}
+                      </div>
+                    </div>
+                  </td>
+                  <td className="p-4 hidden lg:block">{item.description}</td>
+                  <td className="p-4 text-center">${item.price}</td>
+                  <td className="p-4 text-center">{item.quantity}</td>
+                  <td className="p-4 text-center flex flex-col sm:flex-row sm:space-x-2 justify-center">
+                    <button
+                      className="bg-blue-500 text-white rounded-full h-10 w-10 flex justify-center items-center hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-700"
+                      onClick={() => handleQuantityChange(item.id, true)}
+                    >
+                      +
+                    </button>
+                    <button
+                      className="bg-red-500 text-white rounded-full h-10 w-10 flex justify-center items-center hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-700"
+                      onClick={() => handleQuantityChange(item.id, false)}
+                    >
+                      -
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="mt-4 p-4 bg-gray-100 rounded-lg flex justify-between items-center">
+            <h2 className="text-lg font-bold">Total: ${getTotalPrice()}</h2>
+            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+              Proceed to Payment
+            </button>
+          </div>
+        </div>
       ) : (
         <p>No items in your cart.</p>
       )}
